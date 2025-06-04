e4a955a20d23f9f8943c3b43396d3b73169b99f9
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2025-06-04 23:56:14 +0530

    added the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,30 @@ import Loader from "./Components/Loader";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Events from "./Pages/Events";
+import Layout from "./Components/Layout";
+import Gallery from "./Pages/Gallery";
+import Members from "./Pages/Members";
+import About from "./Pages/About";
+import Schedule from "./Pages/Schedule";
+import Sponsors from "./Pages/Sponsors";
 
 function App() {
   
 
   return (
     <Router>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
