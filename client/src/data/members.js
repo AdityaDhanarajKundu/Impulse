62dcde89ff3d0085a45aea6698d8fc10abae81f8
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2025-06-04 23:56:29 +0530

    list of members as  per designations

---
+++ client/src/data/members.js
@@ -0,0 +1,484 @@
+export const excutiveHeads = [
+  {
+    email: "biswasatanu2001@gmail.com",
+    name: "Atanu Biswas",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749060528/IMG_1635_-_Atanu_Biswas_qjupdq.jpg",
+    designation: "Executive Head",
+    contact: 7980822922,
+  },
+  {
+    email: "berasankab2003@gmail.com",
+    name: "Sankab Bera",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749060531/SankabBera_-_SANKAB_BERA_zycjol.jpg",
+    designation: "Executive Head",
+    contact: 8695100565,
+  },
+  {
+    email: "anweshadas1819@gmail.com",
+    name: "Anwesha Das",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749058156/IMG_20240118_170528_413_-_ANWESHA_DAS_vk8fzc_fxatoq.jpg",
+    designation: "Executive Head",
+    contact: 9547466829,
+  },
+  {
+    email: "bishalmajumder08@gmail.com",
+    name: "Bishal Majumder",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054366/1000048729-01_-_Bishal_Majumder_rbbthu.jpg",
+    designation: "Executive Head",
+    contact: 9163009680,
+  },
+  {
+    email: "manjirasamanta@gmail.com",
+    name: "Manjira Samanta",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054387/IMG_20250517_122238_381_-_Manjira_Samanta_td6kvv.webp",
+    designation: "Executive Head",
+    contact: 9091786612,
+  },
+  {
+    email: "rpan250404@gmail.com",
+    name: "Raima Pan",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749056677/Raima_Pan_-_Raima_Pan_vwmmoa_eg4n9j.jpg",
+    designation: "Executive Head",
+    contact: 8910756949,
+  },
+  {
+    email: "bikrammandal2002@gmail.com",
+    name: "Bikram Mandal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054374/Bikram_Mandal_-_Bikram_Mandal_amjcui.jpg",
+    designation: "Executive Head",
+    contact: 7439998997,
+  },
+  {
+    email: "dipakkumarpal777@gmail.com",
+    name: "Dipak Kumar Pal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054376/Dipak_Kumar_Pal_-_Dipak_Kumar_Pal_jgd1ly.jpg",
+    designation: "Executive Head",
+    contact: 8768847881,
+  },
+  {
+    email: "nitishkrmahata21@gmail.com",
+    name: "Nitish Kumar Mahata",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054379/IMG-20240118-WA0042_-_Nitish_Kumar_Mahata_ni0jcl.jpg",
+    designation: "Executive Head",
+    contact: 767938759,
+  },
+  {
+    email: "chakrabortysoumitri@gmail.com",
+    name: "Soumitri Chakraborty",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054396/Soumitri_Chakraborty_-_soumitri_chakraborty_cxeioh.jpg",
+    designation: "Executive Head",
+    contact: 9073396585,
+  },
+  {
+    email: "halderarnab508@gmail.com",
+    name: "Arnab Halder",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054373/Arnab_Halder_-_Arnab_Halder_svxy90.jpg",
+    designation: "Executive Head",
+    contact: 9832042758,
+  },
+  {
+    email: "anyketgupta2002@gmail.com",
+    name: "Aniket Gupta",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054372/Aniket_Gupta_-_Aniket_Gupta_ekemyz.png",
+    designation: "Executive Head",
+    contact: 6297977102,
+  },
+  {
+    email: "sonaisar1602@gmail.com",
+    name: "Srija Sar",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054386/IMG20250503124244_-_Srija_Sar_c2bjbz.jpg",
+    designation: "Executive Head",
+    contact: 9547828870,
+  },
+  {
+    email: "rounaksarkar20@gmail.com",
+    name: "Rounak Sarkar",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054387/IMG_20240118_142640_-_Rounak_Sarkar_jsxjnl.jpg",
+    designation: "Executive Head",
+    contact: 8918827249,
+  },
+  {
+    email: "ishitajha76@gmail.com",
+    name: "Ishita Jha",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749058653/IMG-20240307-WA0005_-_Ishita_Jha_c5kcdx_r3904u.jpg",
+    designation: "Executive Head",
+    contact: 8290698269,
+  },
+  {
+    email: "semantiraul@gmail.com",
+    name: "Semanti Raul",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054379/IMG-20240118-WA0109_-_Semanti_Raul_ot6dvi.jpg",
+    designation: "Executive Head",
+    contact: 8972762328,
+  },
+  {
+    email: "dishamondal31665@gmail.com",
+    name: "Disha Mondal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054368/20240311_105024_-_Disha_Mondal_mndbly.jpg",
+    designation: "Executive Head",
+    contact: 9735922717,
+  },
+  {
+    email: "sreeparnadebbarman@gmail.com",
+    name: "Sreeparna Deb Barman",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054375/DOC-20250604-WA0002._-_SREEPARNA_DEB_BARMAN_m6pgew.jpg",
+    designation: "Executive Head",
+    contact: 8100487885,
+  },
+  {
+    email: "iamsnigdha2003@gmail.com",
+    name: "Snigdha Hati",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054369/20250604_001824_-_Snigdha_Hati_lxmuie.jpg",
+    designation: "Executive Head",
+    contact: 9434645711,
+  },
+  {
+    email: "ksbharti0522@gmail.com",
+    name: "Shruti Bharti",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054384/IMG-20250604-WA0008_2_-_Shruti_Bharti_cmjkgj.jpg",
+    designation: "Executive Head",
+    contact: 8700436522,
+  },
+  {
+    email: "prantikmondal37@gmail.com",
+    name: "Prantik Mondal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054393/Prantik_Mondal_-_Prantik_Mondal_cibmg0.jpg",
+    designation: "Executive Head",
+    contact: 7003254012,
+  },
+  {
+    email: "helloankurmeena@gmail.com",
+    name: "Ankur Meena",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054372/ANKUR_MEENA_-_Ankur_lzymgg.jpg",
+    designation: "Executive Head",
+    contact: 8503072545,
+  },
+  {
+    email: "rohit.podder2002@gmail.com",
+    name: "Rohit Podder",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054389/IMG_2222_-_Rohit_Podder_mlqev7.jpg",
+    designation: "Executive Head",
+    contact: 7439586173,
+  },
+  {
+    email: "bappapratihar21@gmail.com",
+    name: "Bappa Pratihar ",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054367/1737356982810_-_bappa_Pratihar_obz735.jpg",
+    designation: "Executive Head",
+    contact: 8927280075,
+  },
+];
+
+export const culturalHeads = [
+  {
+    email: "nmishra8961@gmail.com",
+    name: "Nidhi Mishra",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749060530/Nidhi_Mishra_-_NIDHI_MISHRA_xmgqns.jpg",
+    designation: "Cultural Head",
+    contact: 8100615597,
+  },
+  {
+    email: "niveditaa03@gmail.com",
+    name: "Nivedita Dhibar",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054391/NIVEDITA__DHIBAR_-_Nivedita_Dhibar_mkp7hv.jpg",
+    designation: "Cultural Head",
+    contact: 8637012132,
+  },
+  {
+    email: "anweshabasu9733@gmail.com",
+    name: "Anwesha Basu",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054373/Anwesha_Basu_jpg_-_Anwesha_Basu_lwhrj0.jpg",
+    designation: "Cultural Head",
+    contact: 8328739979,
+  },
+  {
+    email: "srishtibarman25@gmail.com",
+    name: "Srishti Barman",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054398/Srishti_Barman_-_srishti_oww0bf.jpg",
+    designation: "Cultural Head",
+    contact: 7384636499,
+  },
+  {
+    email: "subhadeep.rc.14@gmail.com",
+    name: "Subhadeep Roy Choudhury",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054380/IMG-20240819-WA0027_-_Subhadeep_SRC_mmnwor.jpg",
+    designation: "Cultural Head",
+    contact: 7980848166,
+  },
+  {
+    email: "sarkarpragatiranjan@gmail.com",
+    name: "Pragati Ranjan Sarkar",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054391/Pragati_Ranjan_Sarkar_-_Pragati_Ranjan_Sarkar_azclaq.jpg",
+    designation: "Cultural Head",
+    contact: 7811073804,
+  },
+  {
+    email: "swagatasinhabnk2@gmail.com",
+    name: "Swagata Sinha",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054382/IMG-20250516-WA0083_-_Swagata_Sinha_kwqjmw.jpg",
+    designation: "Cultural Head",
+    contact: 7501841540,
+  },
+  {
+    email: "mehakrana2507@gmail.com",
+    name: "Sanskriti Rana",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749055372/Screenshot_2025-06-04-00-05-49-179_com.miui.gallery_-_Mehak_Rana_oglh2e_zvily2.jpg",
+    designation: "Cultural Head",
+    contact: 9549229607,
+  },
+  {
+    email: "ananyabanerjee220@gmail.com",
+    name: "Ananya Banerjee",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054371/Ananya_Banerjee_-_Ananya_Banerjee_yg37re.jpg",
+    designation: "Cultural Head",
+    contact: 9382438424,
+  },
+  {
+    email: "dipanwitadutta908@gmail.com",
+    name: "Dipanwita Dutta",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749057682/IMG_20250208_211504_-_Dipanwita_Dutta_emwxux_rsatys.jpg",
+    designation: "Cultural Head",
+    contact: 9775508133,
+  },
+  {
+    email: "godhulimanna2020@gmail.com",
+    name: "Godhuli Manna",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054378/Godhuli_Manna_-_GODHULI_MANNA_z4xuie.jpg",
+    designation: "Cultural Head",
+    contact: 6289973737,
+  },
+  {
+    email: "adifferentritu@gmail.com",
+    name: "Subhechha Patra",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054380/IMG-20250209-WA0175_-_Ritu_lyto6x.jpg",
+    designation: "Cultural Head",
+    contact: 7076261041,
+  },
+];
+
+export const technicalHeads = [
+  {
+    email: "rupam151170@gmail.com",
+    name: "Rupam Rana",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054393/Rupam_Rana_-_Rupam_Rana_o4mnom.jpg",
+    designation: "Technical Head",
+    contact: 9062662324,
+  },
+  {
+    email: "garghya9@gmail.com",
+    name: "Arghya Ghosh",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054367/1000385045_-_Arghya_Ghosh_ltz5tr.jpg",
+    designation: "Technical Head",
+    contact: 7501748054,
+  },
+];
+
+export const jointSecretaries = [
+  {
+    email: "dasmahapatrabikram166@gmail.com",
+    name: "Bikram Das Mahapatra",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749055558/Screenshot_2025-06-03-20-09-24-465_com.whatsapp_-_Bikram_Das_Mahapatra_z9hqbl_odyzfg.jpg",
+    designation: "Joint Secretary",
+    contact: 9382021939,
+  },
+  {
+    email: "asishmondal81025@gmail.com",
+    name: "Asish Mondal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054368/1743578426434-01_-_Asish_Mondal_ndjrh7.jpg",
+    designation: "Joint Secretary",
+    contact: 9382145979,
+  },
+];
+
+export const assistantSecretaries = [
+  {
+    email: "aishwaryabiswas03@gmail.com",
+    name: "Aishwarya Biswas",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054370/Aishwarya_Biswas_-_Aishwarya_Biswas_rtesgi.jpg",
+    designation: "Assistant Secretary",
+    contact: 9339099857,
+  },
+  {
+    email: "mandisourav20@gmail.com",
+    name: "Sourav Mandi",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054398/Sourav_Mandi_-_Sourav_Mandi_suamoo.jpg",
+    designation: "Assistant Secretary",
+    contact: 8927481881,
+  },
+];
+
+export const sportsHeads = [
+  {
+    email: "dassubhamoy2001@gmail.com",
+    name: "Subhamoy Das",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749060533/1726071433477_-_Subhamoy_Das_viuwyp.jpg",
+    designation: "Sports Head",
+    contact: 9735006739,
+  },
+  {
+    email: "rahul02patra@gmail.com",
+    name: "Rahul Patra",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054376/FB_IMG_1748962811939_-_Rahul_Patra_yvhb2q.jpg",
+    designation: "Sports Head",
+    contact: 7557001250,
+  },
+  {
+    email: "mss.fatima2001@gmail.com",
+    name: "Fatima Islam",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054388/IMG_20250603_222431_-_Fatima_Islam_lqdznr.jpg",
+    designation: "Sports Head",
+    contact: 6398037569,
+  },
+  {
+    email: "runjhun2404@gmail.com",
+    name: "Runjhun",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054383/IMG-20250603-WA0040_-_Runjhun_sli4vh.jpg",
+    designation: "Sports Head",
+    contact: 8293766996,
+  },
+  {
+    email: "itsmahathir01@gmail.com",
+    name: "Md Mahathir Islam",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054378/FB_IMG_1748975356212_-_md_mahathir_islam_qlcngu.jpg",
+    designation: "Sports Head",
+    contact: null,
+  },
+  {
+    email: "akankshaanshumali@gmail.com",
+    name: "Akanksha Anshumali",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054370/Akanksha_anshumali.jpg_-_Akanksha_Anshumali_vhbpdh.jpg",
+    designation: "Sports Head",
+    contact: 8210060997,
+  },
+  {
+    email: "isharani.jha@gmail.com",
+    name: "Isharani",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054389/IMG_20250604_003128_-_Isharani_z2inev.jpg",
+    designation: "Sports Head",
+    contact: 9654456599,
+  },
+];
+
+export const treasurers = [
+  {
+    email: "amareshmondal1920@gmail.com",
+    name: "Amaresh Mondal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054383/IMG-20250603-WA0000_-_Amaresh_Mondal_syswqp.jpg",
+    designation: "Joint Treasurer",
+    contact: 7679754424,
+  },
+  {
+    email: "hoquerabiul832@gmail.com",
+    name: "Kabirul islam",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054366/1000181165-03_-_Rabiul_Hoque_e14y9y.jpg",
+    designation: "Joint Treasurer",
+    contact: null,
+  },
+  {
+    email: "shivamchakraborty35a@gmail.com",
+    name: "Shivam Chakraborty",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054395/Shivam_chakraborty.jpg_-_Shivam_Chakraborty_wzo7dy.jpg",
+    designation: "Joint Treasurer",
+    contact: 9735877148,
+  },
+  {
+    email: "aj143subgm@gmail.com",
+    name: "Washikur Rahaman",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054400/washikur__rahaman_-_Washikur_Rahaman_idjru1.jpg",
+    designation: "Joint Treasurer",
+    contact: 9564275872,
+  },
+  {
+    email: "bithikapaul2020@gmail.com",
+    name: "Anamika Paul",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749059408/Anamika_Paul_-_Anamika_Paul_gab3jz_qqamfp.jpg",
+    designation: "Joint Treasurer",
+    contact: 6295383318,
+  },
+  {
+    email: "adrirajmandal25@gmail.com",
+    name: "Adriraj Mandal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054370/Adriraj_Mandal_-_ADRIRAJ_MANDAL_zjp5jf.jpg",
+    designation: "Joint Treasurer",
+    contact: 8900085561,
+  },
+  {
+    email: "dr.rajshekhar.kundu007@gmail.com",
+    name: "Rajshekhar Kundu",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054384/IMG-20250604-WA0033_-_Dr.Rajshekhar_Kundu_cc9jh2.jpg",
+    designation: "Joint Treasurer",
+    contact: 7439401780,
+  },
+  {
+    email: "kousik9093mondal@gmail.com",
+    name: "Kousik Mondal",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054381/IMG-20250516-WA0040_1_-_Kousik_Mondal_u6swjt.jpg",
+    designation: "Joint Treasurer",
+    contact: 7431933257,
+  },
+  {
+    email: "vy410147@gmail.com",
+    name: "Vikash yadav",
+    photo:
+      "https://res.cloudinary.com/dtical8ob/image/upload/v1749054366/1000245377_2_-_VIKASH_KUMAR_YADAV_wldp14.jpg",
+    designation: "Joint Treasurer",
+    contact: 9982981387,
+  },
+];
