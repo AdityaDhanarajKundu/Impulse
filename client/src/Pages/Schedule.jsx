8d9ddf4e676f8e72ccf6fde3179ff7ecc07ec897
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2025-06-07 01:36:08 +0530

    schedule page to be decided

--- client/src/Pages/Schedule.jsx
+++ client/src/Pages/Schedule.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import Loader from '../Components/Loader';
+import React from "react";
+import scheduleBg from "../assets/schedule.mp4";
 
 function Schedule() {
   return (
-    <div>
-      <Loader />
-    </div>
-  )
+    <section className="relative min-h-screen w-full overflow-hidden">
+      <video
+        src={scheduleBg}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+      />
+      <div class="relative z-10 mt-50 bg-gray-100 p-8 rounded-xl shadow-md text-center">
+        <h1 class="text-gray-700 text-4xl font-semibold">To Be Decided</h1>
+      </div>
+    </section>
+  );
 }
 
 export default Schedule;
